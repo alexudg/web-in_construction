32604172e015c9b755a5825ad3091a2f13e1fc5c
alexudg <56275818+alexudg@users.noreply.github.com>
2022-04-13 14:44:44 -0500

    update

--- src/js/input.js
+++ src/js/input.js
@@ -1,3 +1,9 @@
 import './header.js';
 
-document.querySelectorAll('.btnInput')[0].classList.add('selected');
+document.querySelector('.btnInput').classList.add('selected');
+
+btnClickme.onclick = async () => {
+    console.log('onclick')
+    const data = await fetch('http://gucitex.com.mx/test.php').then(res => res.text())
+    console.log('res:', data)
+}
