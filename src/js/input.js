2d6fbfb4c0cd48f4ebe58a0c60ea3508df131ff5
alexudg <56275818+alexudg@users.noreply.github.com>
2022-04-16 11:18:17 -0500

    upload

--- src/js/input.js
+++ src/js/input.js
@@ -3,19 +3,7 @@ import './header.js';
 document.querySelector('.btnInput').classList.add('selected');
 
 btnClickme.onclick = async () => {
-    console.log('onclick')
-
-    //const data = await fetch('http://gucitex.com.mx/web-damaxboutique/backend/index.php?fn=getAllCategories')
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Authorization': '0eab84ea-917d-4817-9e33-c98caecf626d'
-    //     }
-    // }).then(res => res.text())
-    // .then(text => {
-    //     console.log(text)
-    //     return text
-    // })
-    // .catch(error => console.log('error:', error))
+    console.log('onclick')    
 
     //const data = await fetch('https://cors-anywhere.herokuapp.com/http://www.gucitex.com.mx/web-damaxboutique/backend/index.php?fn=getAllCategories',
     const data = await fetch('http://www.gucitex.com.mx/web-damaxboutique/backend/index.php?fn=getAllCategories',
